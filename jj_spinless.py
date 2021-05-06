3778dd29c6def9ec782a859e6e58100f38982d05
Simon Reinhardt <simon.reinhardt@physik.uni-r.de>
2021-05-06 19:12:38 +0200

    jj spinless

--- jj_spinless.py
+++ jj_spinless.py
@@ -14,63 +14,22 @@ import scipy.constants as const
 import pathlib
 import time
 
-sigma_0 = tinyarray.array([[1, 0], [0, 1]])
-sigma_x = tinyarray.array([[0, 1], [1, 0]])
-sigma_y = tinyarray.array([[0, -1j], [1j,0]])
-sigma_z = tinyarray.array([[1, 0], [0,-1]])
-
-date_string = datetime.now()
-date_string = date_string.strftime("%Y-%m-%d_%H-%M-%S")
-data_folder = date_string + "_JJ"
-
-print("data folder: ", data_folder)
-pathlib.Path(data_folder).mkdir()
-
-    
-
-def make_syst(m = 0.03 * const.m_e, a=5e-9, W=10, L=10, junction_length=-0.1e-9):
-    t = const.hbar**2 / (2 * m * a**2)
-    print(t)
-    
-    lat = kwant.lattice.square(a, norbs=2)
-    
-
-    syst = kwant.Builder()
-
-    #
-    # H_0
-    #
-    
-    # On-site
-    def onsite(site, mu, Gap, delta_phi):
-        (x, y) = site.pos
-        pot = sigma_z * (4*t - mu)
-        if x > (a*L)/2:
-            delta_phi = -delta_phi
-        if x < (a*L - junction_length) / 2 or x > (a*L + junction_length)/2:
-            pot = pot + Gap * (sigma_x * np.cos(delta_phi/2) - sigma_y * np.sin(delta_phi/2))
-        return pot
-
-    
-    syst[(lat(x,y) for x in range(L) for y in range(W))] = onsite
-
-    
-    # Hoppings
-    syst[lat.neighbors()] = -t * sigma_z
-
-    # Need periodic boundaries x=L-1 <-> x = 0
-    for j in range(W):
-        syst[lat(0,j), lat(L-1,j)] = -t * sigma_z
-
-    
-    # kwant.plot(syst)
-    return syst.finalized()
-
+# lattice constant for discretization
 a = 5e-9
-W = 300
-width = a * W
-print("width = %.3g μm" % (width * 1e6))
-L = 600 + 1
+
+# width of josephson junction
+width = 3e-6
+
+
+electrode_length = 3e-6
+
+junction_length = 100e-9
+
+
+print("width = %.3g μm")
+
+Gap = 200e-6 * const.e
+
 n_phi = 20
 n_s = 1e16
 m_eff = 0.03 * const.m_e
@@ -80,21 +39,89 @@ lambda_fermi = 2 * np.pi / k_fermi
 mu = E_fermi
 n_bound_states = 2 * width / lambda_fermi # with spin: 4 * width / lambda_fermi
 
+sigma_0 = tinyarray.array([[1, 0], [0, 1]])
+sigma_x = tinyarray.array([[0, 1], [1, 0]])
+sigma_y = tinyarray.array([[0, -1j], [1j,0]])
+sigma_z = tinyarray.array([[1, 0], [0,-1]])
+
+date_string = datetime.now()
+date_string = date_string.strftime("%Y-%m-%d_%H-%M-%S")
+data_folder = date_string + "_JJ_width=%g_junction_length=%g_electrode_length=%g" % (width, junction_length, electrode_length)
+
+print("data folder: ", data_folder)
+pathlib.Path(data_folder).mkdir()
+
+    
+
+def make_syst(m = 0.03 * const.m_e, a=5e-9, width=3e-6,
+              electrode_length = 3e-6, junction_length=100e-9,
+              mu=0, Gap=0, delta_phi=0):
+    t = const.hbar**2 / (2 * m * a**2)
+    print(f"m = {m}, a = {a}, width = {width}, electrode_length = {electrode_length}, junction_length = {junction_length}, t = {t}")
+    W = int(width/a)
+    L = int((2*electrode_length + junction_length) / a)
+    L_junction = int(junction_length/a)
+    print(f"L = {L}, W = {W}, L_junction = {L_junction}")
+    lat = kwant.lattice.square(1)
+    
+
+    syst = kwant.Builder()
+
+    #
+    # H_0
+    #
+    
+    # On-site
+    def onsite(site):
+        (x, y) = site.pos
+        pot = sigma_z * (4*t - mu)
+        dphi = delta_phi
+        
+        if x > L/2:
+            dphi = -dphi
+
+        start_junction = int(L_junction - junction_length) / 2
+        if x < start_junction or x >= start_junction + junction_length:
+            pot = pot + Gap * (sigma_x * np.cos(dphi/2) - sigma_y * np.sin(dphi/2))
+        return pot
+    
+    syst[(lat(x,y) for x in range(L) for y in range(W))] = onsite
+
+    
+    # Hoppings
+    syst[lat.neighbors()] = -t * sigma_z
+    
+    # # Need periodic boundaries x=L-1 <-> x = 0
+    # for j in range(W):
+    #     syst[lat(0,j), lat(L-1,j)] = -t * sigma_z
+    def onsite_00(site):
+        return np.abs(onsite(site)[0,0])
+    def onsite_01(site):
+        return np.abs(onsite(site)[1, 0])
+    
+    # kwant.plot(syst,site_color=onsite_00)
+    # kwant.plot(syst,site_color=onsite_01)
+    
+    return syst.finalized()
+
+
 print("E_fermi = %.3g meV, λ_fermi = %.3g nm, N0 = %.2g" % (1000 * mu / const.e, lambda_fermi * 1e9, n_bound_states))
 
-L = L+1
-Gap = 200e-6 * const.e
-junction_length = 50e-9
 
-syst = make_syst(a=5e-9, L=L, W=W, junction_length=junction_length)
 
 phi_vals = np.linspace(-0.1*np.pi, 1.1*np.pi, n_phi)
 energies = []
 free_energies = []
-for phi in phi_vals:
-    t1 = time.time()
+gaps = np.linspace(0, Gap, 5)
+for Gap in gaps:
+    phi = 0
     print("phi = %.3g π" % (phi/np.pi))
-    ham_mat = syst.hamiltonian_submatrix(params=dict(Gap=Gap, mu=mu, delta_phi=phi), sparse=True)
+    print("make syst")
+    t0 = time.time()
+    syst = make_syst(a=5e-9, width=width, electrode_length=electrode_length, junction_length=junction_length, mu=mu, Gap=Gap, delta_phi=phi)
+    t1 = time.time()
+    print("time for make_syst: %.3g" % (t1 - t0))
+    ham_mat = syst.hamiltonian_submatrix(sparse=True)
     ham_mat = ham_mat.tocsc()
 
     k= int(n_bound_states)
@@ -103,8 +130,13 @@ for phi in phi_vals:
     energies.append(evs)
     free_energies.append(-np.sum(evs))
     print("execution time: %.1f s" % (time.time() - t1))
+
 free_energies = 2 * np.array(free_energies) # include 2 for spin
 energies = np.array(energies)
+
+plt.plot(gaps / Gap , energies/Gap)
+plt.show()
+
 current = 2 * const.e / const.hbar * np.gradient(free_energies)
 
 current_modes = current * const.hbar / (const.e * Gap)
@@ -113,8 +145,7 @@ current_modes = current * const.hbar / (const.e * Gap)
 plt.grid()
 plt.xlabel('φ/π')
 plt.plot(phi_vals/np.pi, energies/Gap)
-data_title = data_folder + "/L=%d,W=%d,L_junction=%d_" % (L, W, junction_length)
-plt.savefig(data_title + 'energies.pdf')
+plt.savefig(data_folder + '/energies.pdf')
 
 plt.clf()
 
@@ -126,11 +157,11 @@ plt.ylabel('current (μA)')
 
 plt.plot(phi_vals/np.pi, current*1e6, label="W = %d, L = %d, Δ = %.3g meV, μ = %.3g meV" % (W, L, 1000 * Gap / const.e, 1000 * mu / const.e))
 plt.legend()
-plt.savefig(data_title + 'current-phase.pdf')
+plt.savefig(data_folder + '/current-phase.pdf')
 
 data_block = np.array([phi_vals, free_energies, current, current_modes]).T
 data_block_header = "phi\t\tfree-energy\t\tcurrent\t\tmodes"
-np.savetxt(data_title + 'current-phase.txt', data_block, fmt="%.17g",
+np.savetxt(data_folder + '/current-phase.txt', data_block, fmt="%.17g",
            header=data_block_header, delimiter="\t\t", footer="\n")
 
 
