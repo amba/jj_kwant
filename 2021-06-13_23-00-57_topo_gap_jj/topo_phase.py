23c1640ed22cb4183b4a39ecc4b764e5ee008a23
simon <simon@hpc.de>
2021-06-15 11:49:08 +0200

    topo dresselhaus

---
+++ 2021-06-13_23-00-57_topo_gap_jj/topo_phase.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python3
+
+import jj_kwant.spectrum
+import scipy.constants as const
+import numpy as np
+import jj_kwant.data
+import time
+import multiprocessing
+
+gap = 100e-6 * const.e
+meVnm = 1e-3 * const.e * 1e-9
+
+#rashba = 20e-3 * const.e * 1e-9 # 20 meV nm
+
+#junction_island_width = 50e-9
+#junction_island_spacing = 100e-9
+args = {
+    'mass': 0.02 * const.m_e,
+    'gap': gap,
+ #   'rashba': rashba,
+    'width': 6e-6,
+    'junction_length': 100e-9,
+    'electrode_length': 1e-6,
+    'a': 5e-9
+}
+
+
+data_file = jj_kwant.data.datafile(folder="topo_gap_jj", params=['phi', 'potential', 'B', 'mu', 'soi', 'diff'], args=args)
+
+
+
+def calc(problem):
+    mu = problem['mu']
+    phi = problem['phi']
+    B = problem['B']
+    soi = problem['soi']
+    
+    potential = 0
+    SOI = np.array([[0, soi], [0, 0]])
+    ham = jj_kwant.spectrum.hamiltonian_jj_2d(
+        a = args['a'],
+        m = args['mass'],
+        gap_potential = potential,
+        gap_potential_shape='cosine',
+        mu = mu,
+        gap = args['gap'],
+        SOI = SOI,
+        width = args['width'],
+        junction_length=args['junction_length'],
+        electrode_length=args['electrode_length'],
+        B = [0, B, 0],
+        delta_phi = phi,
+            #junction_island_width = junction_island_width,
+            #junction_island_spacing = junction_island_spacing,
+            # debug=True
+    );
+     
+    evs = jj_kwant.spectrum.positive_low_energy_spectrum(ham, 20)
+    diff = evs[0] - evs[1]
+    print("evs: ", evs)
+    print("logging evs")    
+    data_file.log(evs, {'phi': phi, 'potential': potential, 'B': B, 'mu': mu, 'soi': soi, 'diff': diff})
+
+
+num_cores = 20
+
+
+if __name__ == '__main__':
+#    phi_vals = (np.pi, )
+    
+    
+    mu_vals = (100e-3 * const.e, )
+
+    B_vals = np.linspace(0,2,100)
+    soi_vals = (20* meVnm,)
+    phi_vals = np.linspace(0, np.pi, 100)
+    
+    problems = []
+    for mu in mu_vals:
+        for soi  in soi_vals:
+            for B in B_vals:
+                for phi in phi_vals:
+                    problems.append({'mu': mu, 'phi': phi, 'B': B, 'soi': soi})
+        
+    with multiprocessing.Pool(num_cores) as p:
+        p.map(calc, problems)
+    
+
+    
+
--- topo_phase.py
+++ topo_phase.py
@@ -8,6 +8,7 @@ import time
 import multiprocessing
 
 gap = 100e-6 * const.e
+meVnm = 1e-3 * const.e * 1e-9
 
 #rashba = 20e-3 * const.e * 1e-9 # 20 meV nm
 
@@ -17,25 +18,25 @@ args = {
     'mass': 0.02 * const.m_e,
     'gap': gap,
  #   'rashba': rashba,
-    'width': 3e-6,
+    'width': 6e-6,
     'junction_length': 100e-9,
-    'electrode_length': 100e-9,
+    'electrode_length': 1e-6,
     'a': 5e-9
 }
 
 
-data_file = jj_kwant.data.datafile(folder="topo_gap_jj", params=['phi', 'potential', 'B', 'mu', 'theta'], args=args)
+data_file = jj_kwant.data.datafile(folder="topo_gap_jj", params=['phi', 'potential', 'B', 'mu', 'soi', 'diff'], args=args)
 
-alpha = 20e-3 * const.e * 1e-9
+
 
 def calc(problem):
     mu = problem['mu']
     phi = problem['phi']
     B = problem['B']
-    theta = problem['theta']
+    soi = problem['soi']
     
     potential = 0
-    SOI = np.array([[0, np.cos(theta)], [np.sin(theta), 0]]) * alpha    
+    SOI = np.array([[0, soi], [0, 0]])
     ham = jj_kwant.spectrum.hamiltonian_jj_2d(
         a = args['a'],
         m = args['mass'],
@@ -54,29 +55,32 @@ def calc(problem):
             # debug=True
     );
      
-    evs = jj_kwant.spectrum.positive_low_energy_spectrum(ham, 2)
+    evs = jj_kwant.spectrum.positive_low_energy_spectrum(ham, 20)
+    diff = evs[0] - evs[1]
     print("evs: ", evs)
     print("logging evs")    
-    data_file.log(evs, {'phi': phi, 'potential': potential, 'B': B, 'mu': mu, 'theta': theta})
+    data_file.log(evs, {'phi': phi, 'potential': potential, 'B': B, 'mu': mu, 'soi': soi, 'diff': diff})
 
 
-num_cores = 10
+num_cores = 20
 
 
 if __name__ == '__main__':
 #    phi_vals = (np.pi, )
-    B = 0.5
-    phi = np.pi
-    mu_vals = np.linspace(-1e-3, 15e-3, 100) * const.e
-    theta_vals = np.linspace(0,np.pi, 30)
-    alpha = 20e-3 *const.e * 1e-9
     
-#    potential_vals = np.linspace(0,0.5*mu,20)
+    
+    mu_vals = (100e-3 * const.e, )
+
+    B_vals = np.linspace(0,2,100)
+    soi_vals = (20* meVnm,)
+    phi_vals = np.linspace(0, np.pi, 100)
+    
     problems = []
     for mu in mu_vals:
-        for theta  in theta_vals:
-
-            problems.append({'mu': mu, 'phi': phi, 'B': B, 'theta': theta})
+        for soi  in soi_vals:
+            for B in B_vals:
+                for phi in phi_vals:
+                    problems.append({'mu': mu, 'phi': phi, 'B': B, 'soi': soi})
         
     with multiprocessing.Pool(num_cores) as p:
         p.map(calc, problems)
