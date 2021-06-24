3c0b7d00f8f3f951a54f076fe1eb10889b335e8f
simon <simon@hpc.de>
2021-06-24 12:02:09 +0200

    topo

--- topo_soi.py
+++ topo_soi.py
@@ -7,9 +7,11 @@ import jj_kwant.data
 import time
 import multiprocessing
 import psutil
+import random
 
 gap = 100e-6 * const.e
 meVnm = 1e-3 * const.e * 1e-9
+num_cores = 50
 
 #rashba = 20e-3 * const.e * 1e-9 # 20 meV nm
 
@@ -19,7 +21,7 @@ args = {
     'mass': 0.02 * const.m_e,
     'gap': gap,
  #   'rashba': rashba,
-    'width': 5e-6,
+    'width': 4e-6,
     'junction_length': 100e-9,
     'electrode_length': 1.5e-6,
     'a': 5e-9
@@ -31,20 +33,27 @@ data_file = jj_kwant.data.datafile(folder="topo_gap_jj", params=['phi', 'potenti
 def wait_for_mem():
     while True:
         mem_percent = psutil.virtual_memory().percent
-        if mem_percent < 0.8:
+        if mem_percent < 90:
             break;
         else:
-            print("cannot start process. used memory is %.g percent" % mem_percent * 100)
-            time.sleep(30)
+            print("cannot start process. used memory is %.1f percent" % mem_percent)
+            time.sleep(300 * random.random())
 
-            
+start_time = time.time()            
 def calc(problem):
     mu = problem['mu']
     phi = problem['phi']
     B = problem['B']
     SOI = problem['soi'] # 2x2 matrix
-    potential = 0.8 * mu
+    potential = 0.85 * mu
 
+
+    # do not start all processes at once
+    if time.time() - start_time < 100:
+        sleep_time = 400 * random.random()
+        print("sleeping for ", sleep_time)
+        time.sleep(sleep_time)
+    
     wait_for_mem()
     
     ham = jj_kwant.spectrum.hamiltonian_jj_2d(
@@ -64,7 +73,9 @@ def calc(problem):
             #junction_island_spacing = junction_island_spacing,
             # debug=True
     );
-     
+
+    wait_for_mem()
+    
     evs = jj_kwant.spectrum.positive_low_energy_spectrum(ham, 2)
     
     
@@ -74,7 +85,7 @@ def calc(problem):
     data_file.log(evs, {'phi': phi, 'potential': potential, 'B': B, 'mu': mu, 's_xx': SOI[0,0], 's_xy': SOI[0,1], 's_yx': SOI[1,0], 's_yy': SOI[1,1], 'diff': diff})
 
 
-num_cores = 30
+
 
 
 if __name__ == '__main__':
@@ -82,15 +93,18 @@ if __name__ == '__main__':
     B = 0.1
     phi = np.pi
     problems = []
-    soi_vals = np.linspace(-1,1,11) * 20 * meVnm
+    soi_vals = np.linspace(0, 1,10) * 20 * meVnm
+    
     for xx in soi_vals:
         for xy in soi_vals:
             for yx in soi_vals:
                 for yy in soi_vals:
                     SOI = np.array([[xx, xy], [yx, yy]])
+                    
                     problems.append({
                         'mu': mu, 'phi': phi, 'B': B,
                         'soi': SOI,
+                        
                         })
 
                     
